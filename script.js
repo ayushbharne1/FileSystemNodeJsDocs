c5aa87b3f05875a674134c86ffd3743a766fc67b
ayush <ayushbharne@gmail.com>
2024-04-27 12:43:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//file system
+const fs = require('node:fs');
+
+// # browser m search krne k liye -  ctrl+f
+
+// node ke docs k file system k file system k callback api copy and paste
+//[,optional hota hai]
+
+//--- CREATE
+
+//fs.writeFile(file, data[, options], callback)
+
+// fs.writeFile('a1.txt',"helo",function(err){
+//     if(err) console.log(err)
+//     else console.log("done")
+// })
+
+//--- READ
+
+//fs.readFile(path[, options], callback)
+
+// fs.readFile('a1.txt','utf-8',function(err , data){
+//     if(err) console.log(err)
+//     else console.log(data)
+// })
+
+//--- APPEND
+
+// fs.appendFile(path, data[, options], callback)
+
+// fs.appendFile("a1.txt","appended", function(err){
+//     if(err) console.log(err)
+//     else console.log("appended")
+// })
+
+
+//--- DELETE
+
+fs.unlink('a.txt',function(err){
+    if(err) throw err;
+    else console.log('done')
+})
